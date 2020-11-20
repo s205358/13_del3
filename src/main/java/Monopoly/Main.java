06a4b0b4564213614f2d8c4f60626632bada94a5
s205358 <s205358@student.dtu.dk>
2020-11-20 14:36:59 +0100

    Runnable

---
+++ src/main/java/Monopoly/Main.java
@@ -0,0 +1,10 @@
+package Monopoly;
+
+public class Main {
+    private static Game game = new Game();
+
+    public static void main(String[] args) {
+        game.init();
+        game.play();
+    }
+}
