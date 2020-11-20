b15ab59a758b35f8ffbbe7a4277d904d02de31c4
s205358 <s205358@student.dtu.dk>
2020-11-20 11:53:25 +0100

    Created Square package, made Square abstract, moved Square

---
+++ src/main/java/Monopoly/Squares/Square.java
@@ -0,0 +1,25 @@
+package Monopoly.Squares;
+
+public abstract class Square {
+    private String name;
+
+    public Square(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
