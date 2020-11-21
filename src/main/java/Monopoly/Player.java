e77e2c1d695e64c5a52637ed94530cddd9f3300d
s205358 <s205358@student.dtu.dk>
2020-11-21 13:41:42 +0100

    Updated Player class with new methods for owned properties

--- src/main/java/Monopoly/Player.java
+++ src/main/java/Monopoly/Player.java
@@ -1,9 +1,15 @@
 package Monopoly;
 
+import Monopoly.Squares.Property;
+
+import java.util.ArrayList;
+
 public class Player {
     private String name;
     private Bank bank;
     private Piece piece;
+    private ArrayList<Property> properties = new ArrayList<Property>();
+
     private boolean paidBail = false;
     private boolean exCon = false;
 
@@ -11,6 +17,60 @@ public class Player {
         this.name = name;
         this.bank = new Bank(balance);
         this.piece = new Piece(location);
+    }
+
+    public boolean forcedSale(int debt) {
+        if (this.bank.getBalance() < debt && hasProperties()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String listProperties() {
+        String list = "";
+        int n = 1;
+        for (Property property: properties) {
+           list += (n++) + ": " + property.getName() + " for " + property.getValue() + "\n";
+        }
+        return list;
+    }
+
+    public int numOfProperties() {
+        return properties.size();
+    }
+
+    public boolean hasProperties() {
+        if (properties.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void addProperty(Property property) {
+        this.properties.add(property);
+    }
+
+    public void removeProperty(Property property) {
+        int i = properties.indexOf(property);
+        this.properties.remove(i);
+    }
+
+    public Property getProperty(int i) {
+        return properties.get(i);
+    }
+
+    public void setProperty(int i, Property property) {
+        this.properties.set(i, property);
+    }
+
+    public ArrayList<Property> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(ArrayList<Property> properties) {
+        this.properties = properties;
     }
 
     public void pay(Player recipient, int amount){
