5349174e7e5853867e8b99cc0e994341c45c8572
s195784 <s195784@student.dtu.dk>
2020-11-23 15:30:52 +0100

    Added "Lottery" card

---
+++ src/main/java/Monopoly/Cards/Lottery.java
@@ -0,0 +1,18 @@
+package Monopoly.Cards;
+
+import Monopoly.Player;
+
+public class Lottery extends Card {
+    private int amount;
+
+    public Lottery(String instructions, int amount) {
+        super(instructions);
+        this.amount = amount;
+    }
+
+    @Override
+    public void update(Player player) {
+        player.getBank().deposit(amount);
+    }
+}
+
