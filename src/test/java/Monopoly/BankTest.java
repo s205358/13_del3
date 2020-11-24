c2622ad0ccefa8a9bce9a7ab12ebb93cb2e1269e
s205358 <s205358@student.dtu.dk>
2020-11-24 15:53:41 +0100

    Added JUnit Testing

---
+++ src/test/java/Monopoly/BankTest.java
@@ -0,0 +1,36 @@
+package Monopoly;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankTest {
+
+    Bank bank = new Bank(90);
+
+    @Test
+    void deposit() {
+        bank.deposit(10);
+        assertEquals(100, bank.getBalance());
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void updateBalance() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void setBalance() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
