77abfda66cd5ad7b0698703e53f0b036a9c2a929
s205358 <s205358@student.dtu.dk>
2020-11-24 11:12:08 +0100

    Added Deck class

---
+++ src/main/java/Monopoly/Deck.java
@@ -0,0 +1,22 @@
+package Monopoly;
+
+import Monopoly.Cards.*;
+
+public class Deck {
+    private Card[] cards;
+
+    public Deck() {
+    }
+
+    public Card getRandom() {
+        return cards[(int) (Math.random() * cards.length)];
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+
+    public void setCards(Card[] cards) {
+        this.cards = cards;
+    }
+}
