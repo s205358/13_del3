be3526133a1e2180102f7818d1f35f26600820ad
s195784 <s195784@student.dtu.dk>
2020-11-27 10:51:31 +0100

    added "robbery" test

---
+++ src/test/java/Monopoly/Cards/RobberyTest.java
@@ -0,0 +1,19 @@
+package Monopoly.Cards;
+
+import Monopoly.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RobberyTest {
+
+    Player player = new Player("John Doe", 10,0);
+
+    Robbery robbery = new Robbery("bla",5);
+
+    @Test
+    void update() {
+        robbery.update(player);
+        assertEquals(5, player.getBank().getBalance());
+    }
+}
